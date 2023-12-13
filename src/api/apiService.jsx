b0ccbb3b4133fec2763b95f041c469de34bbbcaf
DFranck <franckdufournet@hotmail.fr>
2023-12-14 02:18:03 +0700

    ajout du mod prod/dev

--- src/api/apiService.jsx
+++ src/api/apiService.jsx
@@ -4,19 +4,23 @@ import userActivityMock from "../mocks/userActivityMock.json";
 import userPerformanceMock from "../mocks/userPerformanceMock.json";
 
 const BASE_URL = "http://localhost:3000";
-
+const mode = "prod";
 export async function getUserInfos(userId) {
   try {
     const response = await fetch(`${BASE_URL}/user/${userId}`);
     const { data } = await response.json();
     return data;
   } catch (error) {
+    if (mode === "dev") {
+      const mockUser = userInfosMock.find((user) => user.id === Number(userId));
+      console.log("userInfosMock", mockUser);
+      return mockUser;
+    }
     console.error(
       "Erreur lors de la récupération des infos utilisateur",
       error
     );
-    const mockUser = userInfosMock.find((user) => user.id === Number(userId));
-    return mockUser;
+    throw error;
   }
 }
 
@@ -26,14 +30,19 @@ export async function getUserActivity(userId) {
     const { data } = await response.json();
     return data;
   } catch (error) {
+    if (mode === "dev") {
+      const mockUser = userActivityMock.find(
+        (user) => user.userId === Number(userId)
+      );
+      // console.log("userActivityMock", mockUser);
+      return mockUser;
+    }
+
     console.error(
       "Erreur lors de la récupération des activités utilisateur",
       error
     );
-    const mockUser = userActivityMock.find(
-      (user) => user.userId === Number(userId)
-    );
-    return mockUser;
+    throw error;
   }
 }
 
@@ -43,14 +52,19 @@ export async function getUserAverageSessions(userId) {
     const { data } = await response.json();
     return data;
   } catch (error) {
+    if (mode === "dev") {
+      const mockUser = userAverageSessionsMock.find(
+        (user) => user.userId === Number(userId)
+      );
+      // console.log("userAverageSessionsMock", mockUser);
+      return mockUser;
+    }
+
     console.error(
       "Erreur lors de la récupération des sessions moyennes utilisateur",
       error
     );
-    const mockUser = userAverageSessionsMock.find(
-      (user) => user.userId === Number(userId)
-    );
-    return mockUser;
+    throw error;
   }
 }
 
@@ -60,13 +74,18 @@ export async function getUserPerformance(userId) {
     const { data } = await response.json();
     return data;
   } catch (error) {
+    if (mode === "dev") {
+      const mockUser = userPerformanceMock.find(
+        (user) => user.userId === Number(userId)
+      );
+      // console.log("userPerformanceMock", mockUser);
+      return mockUser;
+    }
     console.error(
       "Erreur lors de la récupération des performances utilisateur",
       error
     );
-    const mockUser = userPerformanceMock.find(
-      (user) => user.userId === Number(userId)
-    );
-    return mockUser;
+
+    throw error;
   }
 }
--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Dashboard from "./views/Dashboard";
 import "./styles/global.scss";
-
+console.log(process.env.NODE_ENV);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
